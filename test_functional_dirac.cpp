9372fd9a0d905bde36b145b63c18f18d0a174c3f
shw <gabriel.synnaeve@gmail.com>
2011-01-02 02:07:55 +0100

    test lambda func dirac

--- test_functional_dirac.cpp
+++ test_functional_dirac.cpp
@@ -1,4 +1,5 @@
 #include <pl.h>
+#include <iostream>
 
 void test_X_possible(plValues& lambda, const plValues& X_and_Y)
 {
@@ -20,4 +21,20 @@ int main()
     plProbTable P_Y(Y, tmp_table, true);
     plExternalFunction coherence(lambda, X^Y, test_X_possible);
     plFunctionalDirac P_lambda(lambda, X^Y, coherence);
+    plJointDistribution jd(lambda^X^Y, P_X*P_Y*P_lambda);
+    plCndDistribution Cnd_lambda_knowing_X_Y;
+    jd.ask(Cnd_lambda_knowing_X_Y, lambda, X^Y);
+    plValues evidence(X^Y);
+    evidence[X] = true;
+    evidence[Y] = true;
+    plDistribution P_lambda_knowing_X_Y;
+    Cnd_lambda_knowing_X_Y.instantiate(P_lambda_knowing_X_Y, evidence);
+    plDistribution T_P_lambda;
+    P_lambda_knowing_X_Y.compile(T_P_lambda);
+    std::cout << T_P_lambda << std::endl;
+    evidence[X] = false;
+    evidence[Y] = true;
+    Cnd_lambda_knowing_X_Y.instantiate(P_lambda_knowing_X_Y, evidence);
+    P_lambda_knowing_X_Y.compile(T_P_lambda);
+    std::cout << T_P_lambda << std::endl;
 }
