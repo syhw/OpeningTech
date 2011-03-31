6bed5011f0957ff30edc0de6fd8db12b39e04b46
syhw <gabriel.synnaeve@gmail.com>
2011-03-31 20:01:33 +0200

    adding an old test : getBuildings()

---
+++ test_getBuildings.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+#include "replays.h"
+
+using namespace std;
+
+void mapPrint(const multimap<int, Building>& m)
+{
+    for (multimap<int, Building>::const_iterator it = m.begin();
+            it != m.end(); ++it)
+    {
+        cout << "Time: " << it->first << ", Building: " 
+            << it->second << endl;
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    string input;
+    while (cin)
+    {
+        getline(cin, input);
+        string tmpOpening = pruneOpeningVal(input);
+        if (tmpOpening != "")
+        {
+            multimap<int, Building> tmpBuildings;
+            getBuildings(input, tmpBuildings);
+            mapPrint(tmpBuildings);
+        }
+    }
+    return 0;
+}
