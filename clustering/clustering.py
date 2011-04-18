2d24f3885f629e18e3210e24703b2af9fb368c63
syhw <gabriel.synnaeve@gmail.com>
2011-04-18 17:06:58 +0200

    int64 : no longer overflows

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -11,9 +11,18 @@ try:
 except:
     print "You need numpy"
 
-def k_means(t, nbclusters=2, nbiter=2, medoids=False,\
-        distance=lambda x,y: np.linalg.norm(x-y)): # BUG with sqrt and 0.0
-        #distance=lambda x,y: math.sqrt(np.dot(x-y,(x-y).conj()))): # BUG too
+def dist(x, y):
+    s = 0
+    for e in (x-y):
+        s += (e*e)
+    return math.sqrt(s)
+
+def k_means(t, nbclusters=2, nbiter=10, medoids=False,\
+        #distance=lambda x,y: np.linalg.norm(x-y),
+        distance=lambda x,y: math.sqrt(np.dot(x-y,(x-y).conj())), 
+        #distance=dist,
+        responsability=lambda x,c: 1):
+
     """ 
     Each row of t is an observation, each column is a feature 
     'nbclusters' is the number of seeds and so of clusters/centroids 
@@ -42,18 +51,20 @@ def k_means(t, nbclusters=2, nbiter=2, medoids=False,\
         clusters = [[] for c in range(nbclusters)]
         # Step 1: place nbclusters seeds for each features
         centroids = [np.array([random.uniform(min_max[f][0], min_max[f][1])\
-                for f in range(nbfeatures)], np.int32)\
+                for f in range(nbfeatures)], np.int64)\
                 for c in range(nbclusters)]
-        old_centroids = [np.array([-1 for f in range(nbfeatures)], np.int32)\
+        old_centroids = [np.array([-1 for f in range(nbfeatures)], np.int64)\
                 for c in range(nbclusters)] # should not be init, TODO
+        for c in range(nbclusters):
+            print centroids[c], old_centroids[c], centroids[c]-old_centroids[c], np.linalg.norm(centroids[c] - old_centroids[c])
         new_sum = sum([distance(centroids[c], old_centroids[c])\
                 for c in range(nbclusters)])
-        old_sum = 100000000000.0 # TODO clean
+        old_sum = 1000000000 # TODO clean
         np.seterr(invalid='raise')
         # iterate until convergence
         while new_sum < old_sum :
             old_centroids = copy.deepcopy(centroids)
-            print old_sum, new_sum
+            print "k-means iteration, old and new sum: ", old_sum, new_sum
             old_sum = new_sum
             for c in range(nbclusters):
                 clusters[c] = []
@@ -67,7 +78,7 @@ def k_means(t, nbclusters=2, nbiter=2, medoids=False,\
             # Step 3: recalculate the positions of the nbclusters centroids
             for c in range(nbclusters):
                 if medoids:
-                    tmpmin = 100000000000 # TODO clean #
+                    tmpmin = 1000000000 # TODO clean #
                     argmin = 0                         #
                     for o in clusters[c]:              #
                         if tmpdist[o,c] < tmpmin:      #
@@ -75,11 +86,11 @@ def k_means(t, nbclusters=2, nbiter=2, medoids=False,\
                             argmin = o                 #
                     centroids[c] = t[argmin,:]
                 else:
-                    mean = np.array([0 for i in range(nbfeatures)], np.int32)
+                    mean = np.array([0 for i in range(nbfeatures)], np.int64)
                     for o in clusters[c]:
                         mean += t[o,:]
                     mean = map(lambda x: x/len(clusters[c]), mean)
-                    centroids[c] = np.array(mean, np.int32)
+                    centroids[c] = np.array(mean, np.int64)
             new_sum = sum([distance(centroids[c], old_centroids[c])\
                     for c in range(nbclusters)])
         quality = sum([sum([tmpdist[o][c] for o in clusters[c]])\
@@ -120,26 +131,44 @@ def filter_out_undef(tab):
             if e < 0:
                 return False
         return True
-    return np.array([tab[j] for j in range(len(tab))\
-            if not_undef(tab[j])], np.int32)
+    indices = []
+    tmp = []
+    for i in range(len(tab)):
+        if not_undef(tab[i]):
+            indices.append(i)
+            tmp.append(tab[i])
+    return (indices, np.array(tmp, np.int64))
 
 if __name__ == "__main__":
     (template, datalist) = parse(open(sys.argv[1]))
     # ndarray([#lines, #columns], type) and here #columns without label/string
-    data = np.ndarray([len(datalist), len(datalist[0]) - 1], np.int32)
+    data = np.ndarray([len(datalist), len(datalist[0]) - 1], np.int64)
     # transform the kind & dynamic python list into a static numpy.ndarray
     for i in range(len(datalist)):
         for j in range(len(datalist[0]) - 1):
             data[i][j] = datalist[i][j]
-    print k_means(filter_out_undef(data.take(\
-            [template.index("ProtossDarkTemplar")], 1)),\
-            distance = lambda x,y: abs(x-y))
-    print k_means(filter_out_undef(data.take(\
-            [template.index("ProtossFirstExpansion")], 1)),\
-            distance = lambda x,y: abs(x-y))
-    print k_means(filter_out_undef(data.take([\
+#    print k_means(filter_out_undef(data.take(\
+#            [template.index("ProtossDarkTemplar")], 1))[1],\
+#            distance = lambda x,y: abs(x-y))
+#    print k_means(filter_out_undef(data.take(\
+#            [template.index("ProtossFirstExpansion")], 1))[1],\
+#            distance = lambda x,y: abs(x-y))
+    fast_dt_data = filter_out_undef(data.take([\
             template.index("ProtossShuttle"), template.index("ProtossReavor")\
-            ], 1)))
-
-
+            ], 1))
+    fast_dt = k_means(fast_dt_data[1])
+    print fast_dt
+    #print [fast_dt_data[1][i] for i in [fast_dt["clusters"][0]]]
+    #print [fast_dt_data[1][i,0] for i in [fast_dt["clusters"][0]]]
+    #print [fast_dt_data[1][i,1] for i in [fast_dt["clusters"][0]]]
+    import pylab as pl
+    ax = pl.subplot(111)
+    xs = [fast_dt_data[1][i,0] for i in fast_dt["clusters"][0]]
+    ys = [fast_dt_data[1][i,1] for i in fast_dt["clusters"][0]]
+    ax.scatter(xs, ys, s=40, c='b', marker='s', edgecolors='none')
+    xs = [fast_dt_data[1][i,0] for i in fast_dt["clusters"][1]]
+    ys = [fast_dt_data[1][i,1] for i in fast_dt["clusters"][1]]
+    ax.scatter(xs, ys, s=40, c='r', marker='s', edgecolors='none')
+    pl.grid(True)
+    pl.show()
 
