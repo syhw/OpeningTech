689a82cdddf5c66c36e538e8edb3aeed2109ceb1
syhw <gabriel.synnaeve@gmail.com>
2011-04-28 11:41:34 +0200

    noisify script: ./nosify.py tXvX.txt -> noisify it

---
+++ noisify.py
@@ -0,0 +1,18 @@
+﻿#!/opt/local/bin/python
+import sys, random, math
+
+if len(sys.argv) > 1:
+    random.seed()
+    for i in range(0,5):
+        fil = sys.argv[1].split('.')[0]+'n'+str(i)+'.txt'
+        f = open(fil, 'w')
+        for line in open(sys.argv[1]):
+            elems = line.split(';')
+            for n in range(i):
+                elems.pop(int(math.floor(random.uniform(0, len(elems)-0.01))))
+            f.write(';'.join(elems))
+        f.close()
+        print "wrote: ", f
+else:
+    print "need one argument file"
+
